4ac77dd5f773393dcc72dee43b1544357be5f2f7
Поцылуенко <83078431+EvgenPotsulyenko@users.noreply.github.com>
2023-05-15 12:49:11 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package Dz_Display;
+
+public class Main 
+{
+    public static void main(String[] args) {
+        Work work = new Work();
+
+        work.run();
+
+        System.out.println("App is finish");
+    }
+}
---
+++ Work.java
@@ -0,0 +1,48 @@
+package Dz_Display;
+
+// import Dz_Display.IsNum;
+// import Dz_Display.Semaphore;
+
+public class Work implements Runnable
+{
+    Semaphore semaphore;   
+    @Override 
+    public void run() 
+    {
+        IsNum isNum = new IsNum();
+//        System.out.println(" Before Edu ");
+//        isNum.is1();
+        isNum.edu1();
+        isNum.edu2();
+        isNum.edu8();
+
+        double[] led1 = {0,0,1,0,0,1,0,1};
+        double[] led7 = {1,0,1,0,0,1,0,1};
+
+        isNum.test(led1);
+        isNum.test(led7);
+
+//        System.out.println(" After Edu ");
+//        isNum.is1();
+//        isNum.is2();
+//        isNum.is8();
+        // isNum.printAll();
+    }
+
+    private void Sem()
+    {
+        semaphore = new Semaphore();
+        System.out.println(semaphore.neuron);
+
+        semaphore.goRed();
+        semaphore.goGreen();
+
+        System.out.println(" ----- ");
+        for (int i = 0; i < 100; i++) {
+            semaphore.eduStep();
+        }
+        System.out.println(semaphore.neuron);
+        semaphore.goRed();
+        semaphore.goGreen();
+    }
+}
